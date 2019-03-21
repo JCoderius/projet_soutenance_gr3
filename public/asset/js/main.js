559e905abfe971858099ff03df7dcb86b85c881f
JCoderius <jf.hoarau@laposte.net>
2019-03-21 10:04:22 +0100

    init

---
+++ public/asset/js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+
+    $('#flex1').flexslider({
+        animation:"slides"
+
+    },10);
+
+    document.addEventListener('DOMContentLoaded', function(){
+        map = this.getElementById('map'); // je selectionne mon élément global
+        paths = map.getElementsByTagName('path'); // je mets dans un tableau chacunes des formes
+
+        for (var i = 0; i < paths.length; i++) {
+            paths[i].addEventListener("click", function(e){
+                // pour chaque forme, je fais en sorte qu'un click retourne l'id
+                console.log(e.target.id);
+            })
+        }
+    });
+
+
+})
