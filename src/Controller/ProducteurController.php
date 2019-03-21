f27f1256b40202b7dc11d593eaa58e71e960e0ac
Reboux laura <rebouxlaura@gmail.com>
2019-03-21 15:52:17 +0100

    producteur

---
+++ src/Controller/ProducteurController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProducteurController extends AbstractController
+{
+    /**
+     * @Route("/producteur", name="producteur")
+     */
+    public function index()
+    {
+        return $this->render('producteur/index.html.twig', [
+            'controller_name' => 'ProducteurController',
+        ]);
+    }
+}
