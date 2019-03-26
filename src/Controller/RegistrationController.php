e307640f76b3e55f58c8e87fc6100162eedb3cf3
Virginie Tschanhenz <virginie.tschanhenz@orange.fr>
2019-03-26 09:29:41 +0100

    virgin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,8 +36,6 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-
-
             // do anything else you need here, like send an email, or send email validation account
 
             return $guardHandler->authenticateUserAndHandleSuccess(
