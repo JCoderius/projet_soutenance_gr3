db068739774f039c6903546b5fee1fb8c4fbf281
Emmanuel MOUTIER <emmanuelmoutier@gmail.com>
2019-03-29 15:06:02 +0100

    BO ok

--- src/Form/DepartementsType.php
+++ src/Form/DepartementsType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+
 class DepartementsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -21,18 +22,7 @@ class DepartementsType extends AbstractType
             ->add('nom', TextType::class, [
                 'label' =>'Nom du département',
             ])
-            ->add('dep_id', EntityType::class, [
-                // looks for choices from this entity
-                'class' => Departements::class,
-                'label' => 'Départements',
 
-                // uses the User.username property as the visible option string
-                'choice_label' => 'nom',
-
-                // used to render a select box, check boxes or radios
-                // 'multiple' => true,
-                // 'expanded' => true,
-            ])
         ;
     }
 
