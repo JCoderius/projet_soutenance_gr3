9d0cd8073f462ed96824a998fcb69ac7a524fecd
JCoderius <jf.hoarau@laposte.net>
2019-06-04 16:10:08 +0200

    profil categorie maj

--- src/Migrations/Version20190604134034.php
+++ src/Migrations/Version20190604134034.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190604122653 extends AbstractMigration
+final class Version20190604134034 extends AbstractMigration
 {
     public function getDescription() : string
     {
