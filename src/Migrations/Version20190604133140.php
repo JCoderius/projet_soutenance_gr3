ca5bb7c674265d2adc4c82a8cb7bea105352366c
laurareboux <rebouxlaura@gmail.com>
2019-06-04 15:37:07 +0200

    recup

--- src/Migrations/Version20190604133140.php
+++ src/Migrations/Version20190604133140.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190604122653 extends AbstractMigration
+final class Version20190604133140 extends AbstractMigration
 {
     public function getDescription() : string
     {
